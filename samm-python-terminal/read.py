d4bab49b98ccb50e45ac5d6b79d3a433f6665ca3
zhangcong she <86984634+shez12@users.noreply.github.com>
2023-04-08 05:30:40 -0400

    Add files via upload

--- samm-python-terminal/read.py
+++ samm-python-terminal/read.py
@@ -3,6 +3,39 @@ from segment_anything import sam_model_registry, SamPredictor
 import numpy as np
 import matplotlib.pyplot as plt
 
+class segmentator():
+    def __init__(self,sam_checkpoint) -> None:
+
+        # self.sam_checkpoint = "/home/yl/software/segment-anything/notebooks/sam_vit_h_4b8939.pth" 
+        self.sam_checkpoint = sam_checkpoint
+        model_type = "vit_h"
+
+        sam = sam_model_registry[model_type](checkpoint=self.sam_checkpoint)
+        sam.to(device="cuda")
+        self.predictor = SamPredictor(sam)
+        self.predictor.is_image_set = True
+        self.predictor.original_size = (480, 576)
+        self.predictor.input_size = (853, 1024)
+
+    def load_feature(self, folder_path: str):
+        folder_path = "/home/yl/software/mmaptest/slices/segmented_images/segmented_slc98.pkl"
+        with open(folder_path, 'rb') as f:
+            features = pickle.load(f)
+        
+        self.predictor.features = features
+    
+    def predict(self, input_point:np.ndarray, input_label:np.ndarray):
+        masks, scores, logits = self.predictor.predict(
+                                                point_coords=input_point,
+                                                point_labels=input_label,
+                                                multimask_output=True)
+        
+    
+    
+
+
+
+
 def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
